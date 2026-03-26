11578f262035c7b6b026509aaaafa4a5c57fa9c5
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2026-03-26 07:46:11 -0300

    configurando o banco de dados OBS:falta a criação de um definitivo

--- backend/src/main/java/org/cesar/edu/backend/config/CorsConfig.java
+++ backend/src/main/java/org/cesar/edu/backend/config/CorsConfig.java
@@ -1,4 +1,17 @@
 package org.cesar.edu.backend.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 }
