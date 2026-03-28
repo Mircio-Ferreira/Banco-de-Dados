30693cb5d85b1e7f74c4cd6b76773aa615af8877
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2026-03-28 14:49:34 -0300

    adicionando o DAO de categoria de curso

--- backend/src/main/java/org/cesar/edu/backend/repositories/CategoriaRepository.java
+++ backend/src/main/java/org/cesar/edu/backend/repositories/CategoriaRepository.java
@@ -1,4 +1,50 @@
 package org.cesar.edu.backend.repositories;
 
+import org.cesar.edu.backend.models.Categoria;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CategoriaRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CategoriaRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Categoria> categoriaRowMapper = (rs, rowNum) -> {
+        Categoria categoria = new Categoria();
+        categoria.setId_categoria(rs.getLong("id_categoria"));
+        categoria.setNome(rs.getString("nome_da_categoria"));
+        return categoria;
+    };
+
+    public List<Categoria> findAll() {
+        String sql = "SELECT * FROM categoria";
+        return jdbcTemplate.query(sql, categoriaRowMapper);
+    }
+
+    public Categoria findById(Long id) {
+        String sql = "SELECT * FROM categoria WHERE id_categoria = ?";;
+        return jdbcTemplate.queryForObject(sql, categoriaRowMapper, id);
+    }
+
+    public boolean save(Categoria categoria) {
+        String sql = "INSERT INTO categoria (nome_da_categoria) VALUES (?)";
+        return jdbcTemplate.update(sql, categoria.getNome()) > 0;
+    }
+
+    public boolean update(Categoria categoria, Long idAntigo) {
+        String sql = "UPDATE categoria SET nome_da_categoria = ? WHERE id_categoria = ?";
+        return jdbcTemplate.update(sql, categoria.getNome(), idAntigo) > 0;
+    }
+
+    public boolean delete(Long id) {
+        String sql = "DELETE FROM categoria WHERE id_categoria = ?";
+        return jdbcTemplate.update(sql, id) > 0;
+    }
 }
