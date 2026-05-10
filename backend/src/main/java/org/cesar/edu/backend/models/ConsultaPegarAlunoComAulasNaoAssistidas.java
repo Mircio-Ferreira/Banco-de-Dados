474da27ad6615c98be971362e993d12c2135ee76
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2026-05-09 23:00:44 -0300

    adicionando mais uma consulta exigida na entrega 4

---
+++ backend/src/main/java/org/cesar/edu/backend/models/ConsultaPegarAlunoComAulasNaoAssistidas.java
@@ -0,0 +1,13 @@
+package org.cesar.edu.backend.models;
+
+import lombok.*;
+
+@Getter @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ConsultaPegarAlunoComAulasNaoAssistidas {
+    private String cpf;
+    private String nome_curso;
+}
--- backend/src/main/java/org/cesar/edu/backend/repositories/AlunoRepository.java
+++ backend/src/main/java/org/cesar/edu/backend/repositories/AlunoRepository.java
@@ -1,23 +1,28 @@
 package org.cesar.edu.backend.repositories;
 
 import org.cesar.edu.backend.models.Aluno;
+import org.cesar.edu.backend.models.ConsultaPegarAlunoComAulasNaoAssistidas;
 import org.cesar.edu.backend.models.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
 @Repository
 public class AlunoRepository {
     private final JdbcTemplate jdbcTemplate;
     private final UserRepository userRepository;
-    public AlunoRepository(JdbcTemplate jdbcTemplate,  UserRepository userRepository) {
+
+    public AlunoRepository(JdbcTemplate jdbcTemplate, UserRepository userRepository) {
         this.jdbcTemplate = jdbcTemplate;
         this.userRepository = userRepository;
     }
+
     private final RowMapper<Aluno> rowMapper = new RowMapper<Aluno>() {
         @Override
         public Aluno mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -26,17 +31,27 @@ public class AlunoRepository {
             return aluno;
         }
     };
+    private final RowMapper pegarAlunoComAulasNaoAssistidas = new RowMapper<ConsultaPegarAlunoComAulasNaoAssistidas>() {
+        @Override
+        public ConsultaPegarAlunoComAulasNaoAssistidas mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ConsultaPegarAlunoComAulasNaoAssistidas consultaPegarAlunoComAulasNaoAssistidas = new ConsultaPegarAlunoComAulasNaoAssistidas();
+            consultaPegarAlunoComAulasNaoAssistidas.setCpf(rs.getString("cpf_aluno"));
+            consultaPegarAlunoComAulasNaoAssistidas.setNome_curso(rs.getString("nome_curso"));
+            return consultaPegarAlunoComAulasNaoAssistidas;
+        }
+    };
     public List<Aluno> findAll() {
         return jdbcTemplate.query("SELECT * FROM aluno", rowMapper);
     }
+
     public Aluno findByCpf(String cpf) {
         try {
             return jdbcTemplate.queryForObject("SELECT * FROM aluno WHERE cpf_aluno = ?", rowMapper, cpf);
-        }
-        catch (EmptyResultDataAccessException e) {
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
+
     public boolean save(Aluno aluno) {
         boolean salvo = userRepository.save((User) aluno);
         if (salvo) {
@@ -46,17 +61,34 @@ public class AlunoRepository {
             return false;
         }
     }
+
     public boolean update(Aluno aluno, String cpfUserAntigo) {
         boolean salvo = userRepository.update((User) aluno, cpfUserAntigo);
         if (salvo) {
             int linhasAlteradas = jdbcTemplate.update("UPDATE aluno SET cpf_aluno = ? WHERE cpf_aluno = ?", aluno.getCpf(), cpfUserAntigo);
             return linhasAlteradas > 0 ? true : false;
-        }
-        else  {
+        } else {
             return false;
         }
     }
+
     public boolean delete(String cpfAluno) {
         return userRepository.delete(cpfAluno);
+    }
+
+    //retorna cursos que foram comprados mas que não tiveram nenhuma aula visualizada pelo aluno
+    public List<ConsultaPegarAlunoComAulasNaoAssistidas> pegarAlunoComAulasNaoAssistidas() {
+        String sql = """
+                SELECT a.cpf_aluno, c.nome AS nome_curso
+                FROM aluno a
+                JOIN compra co
+                ON a.cpf_aluno = co.cpf_aluno
+                JOIN curso c
+                ON co.id_curso = c.id_curso
+                LEFT JOIN assistir ass
+                ON co.id_curso = ass.id_curso AND co.cpf_aluno = ass.cpf_aluno
+                WHERE ass.id_aula IS NULL;
+                """;
+        return jdbcTemplate.query(sql, pegarAlunoComAulasNaoAssistidas);
     }
 }
